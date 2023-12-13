8087ca1e7fdacc9c97c5b7259c1eb97e1e4b0e0b
HOANGPHI <phi.le@bnksolution.com>
2023-12-13 15:27:21 +0700

    init code

---
+++ components/NavBar/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles.css'
+
+export default function NavBar () {
+  return (
+    <div>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+      </ul>
+    </div>
+  );
+}
